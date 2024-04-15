fbb543da007636ffe20239ff36b80b25471abeea
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-04-15 18:49:40 -0500

    memcpy and forward function

--- test/thread_utils.h
+++ test/thread_utils.h
@@ -30,3 +30,50 @@ void createKWorkers(opRing ***pRings, int numKWorkers, int startCPU){
   }
   *pRings = rings;
 }
+
+/* Stage Operation Functions */
+/* To be called instantiated as a pthread or called in a serial chain test */
+int memcpy_and_forward(struct acctest_context *dsa, struct acctest_context *iaa, int nKWorkers, opRing **ring){
+  int rc;
+  struct task_node *dsa_tsk_node, *iaa_tsk_node;
+
+  host_op_args *args;
+	dsa_tsk_node = dsa->multi_task_node;
+	iaa_tsk_node = iaa->multi_task_node;
+  int idx = 0;
+  while (dsa_tsk_node) {
+			rc = dsa_wait_memcpy(dsa, dsa_tsk_node->tsk);
+			if (rc != ACCTEST_STATUS_OK)
+					pthread_exit((void *)(intptr_t)rc);
+
+      args = malloc(sizeof(host_op_args));
+			if (args == NULL) {
+					pthread_exit((void *)(intptr_t)ENOMEM);  // Handle memory allocation failure
+			}
+			int *(*selected_op)(void *buffer, size_t size) = select_host_op(host_op_sel);
+			if(do_spt_spinup){
+				args->host_op = selected_op;
+				args->buffer = dsa_tsk_node->tsk->dst1;
+				args->size = buf_size;
+				enqueue(ring[idx], args);
+				idx = (idx + 1) % nKWorkers;
+
+			} else {
+				selected_op(dsa_tsk_node->tsk->dst1, buf_size);
+				// no atomic ctr update from thread -- do it here
+				finalHostOpCtr += 1;
+				if(finalHostOpCtr == expectedHostOps){
+					complete = 1;
+				}
+			}
+
+        // Continue with other operations
+        iaa_tsk_node->tsk->src1 = dsa_tsk_node->tsk->dst1;
+        iaa_prep_sub_task_node(iaa, iaa_tsk_node);
+
+        dsa_tsk_node = dsa_tsk_node->next;
+        iaa_tsk_node = iaa_tsk_node->next;
+  }
+  pthread_exit((void *)ACCTEST_STATUS_OK);
+
+}
