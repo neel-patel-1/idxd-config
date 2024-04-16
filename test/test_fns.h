f52feac053c97645e60682488e651e85d733fd16
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-04-16 12:37:35 -0500

    single-iaa-test

--- test/test_fns.h
+++ test/test_fns.h
@@ -45,18 +45,6 @@ int multi_iaa_test(int num_iaas, int tflags, int wq_type, int dev_id, int wq_id,
   }
 
   struct task_node *iaa_tsk_node[num_iaas];
-  clock_gettime(CLOCK_MONOTONIC, &times[0]);
-  for(int i=0; i<num_iaas; i++){
-    iaa_tsk_node[i] = iaa[i]->multi_task_node;
-    while(iaa_tsk_node[i]){
-      iaa_prep_sub_task_node(iaa[i], iaa_tsk_node[i]);
-      iaa_tsk_node[i] = iaa_tsk_node[i]->next;
-
-    }
-  }
-
-
-
   struct completion_record *next_iaa_comp[num_iaas];
 
   /* Submission / work distribution across all iaa instances*/
@@ -89,6 +77,70 @@ int multi_iaa_test(int num_iaas, int tflags, int wq_type, int dev_id, int wq_id,
   }
 
   clock_gettime(CLOCK_MONOTONIC, &times[1]);
+
+}
+
+typedef struct{
+  int tflags;
+  int wq_type;
+  int dev_id;
+  int wq_id;
+  size_t buf_size;
+  int num_desc;
+} iaa_ax_args;
+
+int single_iaa_test( void *arg){
+  /* allocate this iaa's task nodes */
+  int rc;
+  struct acctest_context *iaa;
+  iaa_ax_args *args = (iaa_ax_args *)arg;
+
+  int tflags = args->tflags;
+  int wq_type = args->wq_type;
+  int dev_id = args->dev_id;
+  int wq_id = args->wq_id;
+  size_t buf_size = args->buf_size;
+  int num_desc = args->num_desc;
+  struct task_node *iaa_tsk_node;
+
+  /* setup */
+  iaa = acctest_init(tflags);
+  if (!iaa)
+    return -ENOMEM;
+  rc = acctest_alloc(iaa, wq_type, dev_id, wq_id);
+  if (rc < 0)
+    return -ENOMEM;
+  init_iaa_task_nodes(iaa, buf_size, tflags, num_desc);
+
+  /* submit */
+  while(iaa_tsk_node){
+    iaa_prep_sub_task_node(iaa, iaa_tsk_node);
+    iaa_tsk_node = iaa_tsk_node->next;
+  }
+
+  /* Reset and poll in order */
+  iaa_tsk_node = iaa->multi_task_node;
+  struct completion_record *next_iaa_comp = iaa_tsk_node->tsk->comp;
+  while(iaa_tsk_node){
+    if(next_iaa_comp->status == 1){
+      iaa_tsk_node = iaa_tsk_node->next;
+      next_iaa_comp = iaa_tsk_node->tsk->comp;
+    }
+  }
+
+}
+
+int multi_iaa_bandwidth(int num_wqs, int num_descs){
+  iaa_ax_args *args = malloc(num_wqs * sizeof(iaa_ax_args));
+  for(int i=0; i<num_wqs; i++){
+    args[i].tflags = 0;
+    args[i].wq_type = 0;
+    args[i].dev_id = 1;
+    args[i].wq_id = i;
+    args[i].buf_size = 4096;
+    args[i].num_desc = num_descs;
+  }
+  single_iaa_test(args);
 
 }
 
