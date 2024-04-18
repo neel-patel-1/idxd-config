8faedc77a5a3602a5cf579a62b0b75e20cb27aef
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-04-18 00:06:13 -0500

    case 7

--- test/spt_spinup.c
+++ test/spt_spinup.c
@@ -658,6 +658,9 @@ int main(int argc, char *argv[])
 		case 6:
 			multi_dsa_bandwidth(num_ax, num_desc, buf_size);
 			break;
+		case 7:
+			multi_dsa_dedicated_poller_bandwidth(num_ax, num_desc, buf_size);
+			break;
 		default:
 			printf("No Case\n");
 			exit(-1);
