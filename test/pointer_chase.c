89288adb16e07d1df7972d73884739d8ca281944
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-04-18 18:15:21 -0500

    segfault

--- test/pointer_chase.c
+++ test/pointer_chase.c
@@ -75,11 +75,13 @@ void* pointer_chase_thread(void* arg) {
     void** memory = create_random_chain(memsize);
     size_t count = fmax(memsize * 16, (size_t)1<<30);
     printf("Starting chase pointers\n");
-    double t = chase_pointers(memory, count);
-    free(memory);
-    double ns = t * 1000000000 / count;
-    printf(" %9zu  %10.5lf ns\n", memsize, ns);
-    fflush(stdout);
+
+    while(1){
+        double t = chase_pointers(memory, count);
+        double ns = t * 1000000000 / count;
+        printf(" %9zu  %10.5lf ns\n", memsize, ns);
+        fflush(stdout);
+    }
 
     keep_running = 0;
     return NULL;
@@ -97,12 +99,16 @@ void *wait_for_iaa(void *arg) {
     struct task_node *iaa_tsk_node = iaa->multi_task_node;
     int rc;
 
-    while(iaa_tsk_node) {
-        rc = iaa_wait_compress(iaa, iaa_tsk_node->tsk);
-        if (rc != ACCTEST_STATUS_OK)
-            pthread_exit((void *)(intptr_t)rc);
-        iaa_tsk_node = iaa_tsk_node->next;
+    while (1){
+        iaa_tsk_node = iaa->multi_task_node;
+        while(iaa_tsk_node) {
+            rc = iaa_wait_compress(iaa, iaa_tsk_node->tsk);
+            if (rc != ACCTEST_STATUS_OK)
+                pthread_exit((void *)(intptr_t)rc);
+            iaa_tsk_node = iaa_tsk_node->next;
+        }
     }
+
 
     pthread_exit((void *)ACCTEST_STATUS_OK);
 }
@@ -111,27 +117,32 @@ void feed_iaa(void *arg){
     int tflags = 0x1;
     int buf_size = 4096;
     struct task_node *iaa_tsk_node;
-    int rc = acctest_alloc_multiple_tasks(iaa, 1024);
+    iaa = acctest_init(0x1);
+    int rc = acctest_alloc(iaa, 0, 1, 0);
+    rc = acctest_alloc_multiple_tasks(iaa, 1024);
 	if (rc != ACCTEST_STATUS_OK)
 		return rc;
 
     iaa_tsk_node = iaa->multi_task_node;
-    while(1){
-        while (iaa_tsk_node) {
-            if (rc != ACCTEST_STATUS_OK)
-                return rc;
-            rc = iaa_init_task(iaa_tsk_node->tsk, tflags, IAX_OPCODE_COMPRESS, buf_size);
-            if (rc != ACCTEST_STATUS_OK)
-                return rc;
-            iaa_tsk_node = iaa_tsk_node->next;
-        }
+    while (iaa_tsk_node) {
+        if (rc != ACCTEST_STATUS_OK)
+            return rc;
+        rc = iaa_init_task(iaa_tsk_node->tsk, tflags, IAX_OPCODE_COMPRESS, buf_size);
+        if (rc != ACCTEST_STATUS_OK)
+            return rc;
+        iaa_tsk_node = iaa_tsk_node->next;
+    }
+
+    // while(1){
+
+        iaa_tsk_node = iaa->multi_task_node;
         while(iaa_tsk_node){
             iaa_tsk_node->tsk->src1 = malloc(4096);
+            iaa_tsk_node->tsk->src2 = malloc(4096);
             iaa_prep_sub_task_node(iaa, iaa_tsk_node);
             iaa_tsk_node = iaa_tsk_node->next;
         }
-        iaa_tsk_node = iaa->multi_task_node;
-    }
+    // }
 }
 
 
@@ -158,7 +169,7 @@ int main(int argc, char **argv) {
     if (mode == 2) {
         pthread_attr_init(&attr);
         CPU_ZERO(&cpus);
-        CPU_SET(1, &cpus);
+        CPU_SET(10, &cpus);
         pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
         pthread_create(&iaa_feed, &attr, feed_iaa, NULL);
 
