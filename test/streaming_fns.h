629eeda0bc964c0ec77f19f522a9ef268e9bd201
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-04-23 14:41:02 -0500

    switch to scanning ops

--- test/streaming_fns.h
+++ test/streaming_fns.h
@@ -1,3 +1,4 @@
+#include "iaa_task_init_utils.h"
 static inline void dsa_streaming_submission(struct acctest_context *dsa, int numSubs, int wq_depth){
   int rc;
   int submission_count = 0;
@@ -55,23 +56,26 @@ static inline void iaa_streaming_submission(struct acctest_context *iaa, int num
   struct task_node *start_tsk_node = tsk_node;
 
   /* Prep all task nodes */
-  while (tsk_node) {
-		tsk_node->tsk->dflags |= (IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR);
-		if ((tsk_node->tsk->test_flags & TEST_FLAGS_BOF) && iaa->bof)
-			tsk_node->tsk->dflags |= IDXD_OP_FLAG_BOF;
+  // while (tsk_node) {
+	// 	tsk_node->tsk->dflags |= (IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR);
+	// 	if ((tsk_node->tsk->test_flags & TEST_FLAGS_BOF) && iaa->bof)
+	// 		tsk_node->tsk->dflags |= IDXD_OP_FLAG_BOF;
 
-		tsk_node->tsk->dflags |= (IDXD_OP_FLAG_WR_SRC2_CMPL | IDXD_OP_FLAG_RD_SRC2_AECS);
-		tsk_node->tsk->iaa_src2_xfer_size = IAA_COMPRESS_AECS_SIZE;
+	// 	tsk_node->tsk->dflags |= (IDXD_OP_FLAG_WR_SRC2_CMPL | IDXD_OP_FLAG_RD_SRC2_AECS);
+	// 	tsk_node->tsk->iaa_src2_xfer_size = IAA_COMPRESS_AECS_SIZE;
 
-		memcpy(tsk_node->tsk->src2, (void *)iaa_compress_aecs, IAA_COMPRESS_AECS_SIZE);
+	// 	memcpy(tsk_node->tsk->src2, (void *)iaa_compress_aecs, IAA_COMPRESS_AECS_SIZE);
 
-		tsk_node->tsk->iaa_compr_flags = (IDXD_COMPRESS_FLAG_EOB_BFINAL |
-						  IDXD_COMPRESS_FLAG_FLUSH_OUTPUT);
-		tsk_node->tsk->iaa_max_dst_size = IAA_COMPRESS_MAX_DEST_SIZE;
+	// 	tsk_node->tsk->iaa_compr_flags = (IDXD_COMPRESS_FLAG_EOB_BFINAL |
+	// 					  IDXD_COMPRESS_FLAG_FLUSH_OUTPUT);
+	// 	tsk_node->tsk->iaa_max_dst_size = IAA_COMPRESS_MAX_DEST_SIZE;
 
-		iaa_prep_compress(tsk_node->tsk);
-		tsk_node = tsk_node->next;
-	}
+	// 	iaa_prep_compress(tsk_node->tsk);
+	// 	tsk_node = tsk_node->next;
+	// }
+  iaa_scan_multi_task_nodes(iaa);
+  rc = iaa_task_result_verify_task_nodes(iaa, 0);
+  return
 
   tsk_node = iaa->multi_task_node;
 
@@ -87,7 +91,7 @@ static inline void iaa_streaming_submission(struct acctest_context *iaa, int num
   /* Streaming submission as completions arrive */
   struct task_node *next_to_complete = iaa->multi_task_node;
   while(tsk_node){
-    iaa_wait_compress(iaa, next_to_complete->tsk);
+    iaa_wait_scan(iaa, next_to_complete->tsk);
     next_to_complete = next_to_complete->next;
 		acctest_desc_submit(iaa, tsk_node->tsk->desc);
     tsk_node = tsk_node->next;
@@ -127,6 +131,23 @@ int dsa_streaming_submit(void *args) {
   if (rc != ACCTEST_STATUS_OK)
 		return rc;
   printf("Allocated tasks\n");
+
+  const char* directory_path = "./test/images";
+  char filepath[1024];
+  int image_index = 0;
+
+  struct task_node * iaa_scan_tsk_node = iaa->multi_task_node;
+  while (iaa_scan_tsk_node) {
+    snprintf(filepath, sizeof(filepath), "%s/test_%d.JPEG", directory_path, image_index++);
+        printf("Initializing with image: %s\n", filepath);
+		snprintf(filepath, sizeof(filepath), "%s/test_%d.JPEG", directory_path, image_index++);
+        printf("Initializing with image: %s\n", filepath);
+
+		rc = init_scan_from_image(iaa_scan_tsk_node->tsk, tflags, IAX_OPCODE_SCAN, filepath);
+        if (rc != ACCTEST_STATUS_OK)
+            return rc;
+  }
+
 
   dsa_tsk_node = dsa->multi_task_node;
 	while (dsa_tsk_node) {
@@ -204,9 +225,9 @@ int iaa_streaming_submit(void *args) {
     wq_id, submitDepth, buf_size, (double)buf_size * submitDepth * num_iter / nanos);
 
   rc = iaa_task_result_verify_task_nodes(iaa, 0);
-  // if (rc != ACCTEST_STATUS_OK){
-  //   printf("Failed to verify IAA task nodes\n");
-  // }
+  if (rc != ACCTEST_STATUS_OK){
+    printf("Failed to verify IAA task nodes\n");
+  }
 
   acctest_free_task(iaa);
   acctest_free(iaa);
